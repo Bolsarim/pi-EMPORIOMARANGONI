c5a81c38ba770f5ee8d99994fa83e276982e21e3
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2023-12-05 22:06:28 -0300

    atividade pi

---
+++ app.js
@@ -0,0 +1,10 @@
+function jedi(){
+    document.getElementById("mensagem").innerText = "Venha para o lado da luz"
+    document.getElementById("frases").innerText = "Guerras não fazem ninguem maior"
+    document.getElementById("imagem").src=""
+}
+
+function sitch(){
+    document.getElementById("mensagem").innerText = "Venha para o lado sombrio"
+    document.getElementById("frases").innerText = "A paz é uma mentira, só a emoção importa"
+}
